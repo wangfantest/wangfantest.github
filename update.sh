b2a30242e9cf91b41c474bbcd1526268dd5567ae
TheTame <96096907+fanwang551@users.noreply.github.com>
2023-09-11 16:27:39 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 git checkout --orphan newBranch
 rm -f -r docs
-hugo -b "https://fjqisba.github.io" -d docs
+hugo -b "https://wangfantest.github.io" -d docs
 git add -A
 git commit -am "commit message"
 git branch -D master
